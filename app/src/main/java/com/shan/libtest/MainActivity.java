45c5305baf8e39fcb593f3f5f72afa6d67d8df24
shanjiaxiang <1135630810@qq.com>
2018-12-01 15:28:24 +0800

    alertdialog

--- app/src/main/java/com/shan/libtest/MainActivity.java
+++ app/src/main/java/com/shan/libtest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.shan.libtest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+import x.lib.base.dialog.XSweetDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SweetAlertDialog dialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+        dialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        dialog.setTitleText("loading");
+        dialog.setCancelable(false);
+        dialog.show();
     }
 }
